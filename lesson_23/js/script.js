d2f582d0c3860518ece890268428dea260b8ffef
pou4low <94647662+pou4low@users.noreply.github.com>
2022-12-01 16:36:42 +0200

    Add files via upload

---
+++ lesson_23/js/script.js
@@ -0,0 +1,12 @@
+
+let numFirst = 0;
+while (numFirst < 5) {
+   console.log (numFirst);
+   numFirst++ 
+} 
+for (let numSecond = 0; numSecond < 5; numSecond++) { 
+   console.log(numSecond); 
+}
+
+let num = Math.floor(58.858);
+console.log (num);
